6eb9a2203fa7979743976116135014f223af15b1
Yefta Asyel <yeftakun34@gmail.com>
2023-12-11 07:10:37 +0800

    test

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "3.106.116.196";
+$username = "admin";
+$password = "Aa123456";
 $dbname = "new_perpus";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
